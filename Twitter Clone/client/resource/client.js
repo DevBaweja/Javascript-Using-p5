c3adbd9130b4294c85b734f1c64b673ca5df2cdd
Dev Baweja <18ucs081@lnmiit.ac.in>
2020-05-15 20:44:19 +0530

    twitter clone

--- Twitter Clone/client/resource/client.js
+++ Twitter Clone/client/resource/client.js
@@ -1,9 +1,13 @@
 const form = document.querySelector('.mew-form');
 const loading = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews');
 const API_URL = 'http://localhost:5000/mews';
 
+// Getting all mews
+listAllMews();
+
 // Adding event listener
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
     // Otherwise it will reload
     event.preventDefault();
 
@@ -11,29 +15,95 @@ form.addEventListener('submit', (event) => {
     const name = formData.get('mew-form__name');
     const content = formData.get('mew-form__content');
 
-    if (name !== '' && content !== '') {
-        // Prepare UI
-        document.querySelector('#mew-form__name').value = '';
-        document.querySelector('#mew-form__content').value = '';
+    // Client side validation
+    // if (name !== '' && content !== '')
 
-        form.classList.add('not-visible');
-        loading.classList.remove('not-visible');
+    addLoader();
 
-        const mew = {
-            name,
-            content,
-        };
+    const mew = {
+        name,
+        content,
+    };
 
-        fetch(API_URL, {
-            method: 'POST',
-            mode: 'cors',
-            header: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(mew),
-        });
+    await fetch(API_URL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(mew),
+    })
+        .then((response) => response.json())
+        .then((createdMew) => {
+            console.log(createdMew);
 
-        form.classList.remove('not-visible');
-        loading.classList.add('not-visible');
-    }
+            // Prepare UI
+            // document.querySelector('#mew-form__name').value = '';
+            // document.querySelector('#mew-form__content').value = '';
+            // Build in function
+            form.reset();
+
+            // Again listing all
+            listAllMews();
+        })
+        .catch((error) => console.log('Client side error'));
 });
+
+function addLoader() {
+    form.classList.add('not-visible');
+    loading.classList.remove('not-visible');
+}
+
+function addForm() {
+    form.classList.remove('not-visible');
+    loading.classList.add('not-visible');
+}
+
+async function listAllMews() {
+    mewsElement.innerHTML = '';
+    addLoader();
+    await fetch(API_URL, {
+        method: 'GET',
+        mode: 'cors',
+    })
+        .then((response) => response.json())
+        .then((mews) => {
+            console.log(mews);
+            mews.reverse();
+            let markup = '';
+            mews.forEach((mew) => {
+                markup += `
+            <div>
+                <h3>
+                    ${mew.name}
+                </h3>
+                <p>
+                    ${mew.content}
+                </p>
+                <small>
+                    ${mew.created}
+                </small>
+            </div>
+            `;
+
+                const div = document.createElement('div');
+                const heading = document.createElement('h3');
+                heading.textContent = mew.name;
+
+                const para = document.createElement('p');
+                para.textContent = mew.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(mew.created);
+
+                div.appendChild(heading);
+                div.appendChild(para);
+                div.appendChild(date);
+
+                // mewsElement.insertAdjacentElement('afterbegin',div);
+                // mewsElement.appendChild(div);
+            });
+            mewsElement.insertAdjacentHTML('beforeend', markup);
+            addForm();
+        });
+}
--- Twitter Clone/server/index.js
+++ Twitter Clone/server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const monk = require('monk');
+const Filter = require('bad-words');
+const rateLimit = require('express-rate-limit');
 
+//  apply to all requests
 const app = express();
+const db = monk('localhost/meower');
+const mews = db.get('mews');
+const filter = new Filter();
 
 // Basic Config
 app.use(cors());
@@ -10,20 +17,79 @@ app.use(bodyParser.json());
 
 // app.use(express.json());
 
-app.get('/',(req,res) => {
-
+app.get('/', (req, res) => {
     res.json({
-        message : 'Meower ! 😹'
+        message: 'Meower ! 😹',
     });
-
-}); 
-
-app.post('/mews',(req,res) => {
-    console.log(req);
 });
 
+const limiter = rateLimit({
+    windowMs: 30 * 1000, // 30 second
+    max: 10, // limit each IP to 1 requests per windowMs
+});
+app.use(limiter);
 
 
-app.listen(5000,() => {
+app.get('/mews', (req, res) => {
+    mews.find().then((mews) => {
+        setTimeout(() => {
+            res.json(mews);
+        }, 1000);
+    });
+});
+
+function isValidMew(mew) {
+    return mew.name && mew.toString().trim() !== '' && mew.content && mew.content.toString().trim() !== '';
+}
+
+app.post('/mews', async (req, res) => {
+    // Server side Validation
+    if (isValidMew(req.body)) {
+        // Insert into db
+        const mew = {
+            name: filter.clean(req.body.name.toString()),
+            content: filter.clean(req.body.content.toString()),
+            created: new Date(),
+        };
+
+        await mews
+            .insert(mew)
+            .then((createdMew) => {
+                setTimeout(() => {
+                    res.json(createdMew);
+                }, 1000);
+            })
+            .catch((error) => console.log('Server side Error'));
+    } else {
+        res.status(422);
+        setTimeout(() => {
+            res.json({
+                message: 'Hey Name and Content are required',
+            });
+        }, 1000);
+    }
+});
+
+app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
 });
+
+/*
+For validator of inputs 
+
+express validator
+joi 
+yup
+*/
+
+/*
+const db = monk(process.env.MONGO_URI || 'localhost/meower')
+For Deployment
+mLab
+now secrets add meower-db url
+
+zelt.co
+now -e MONGO_URI=@meower-db
+
+now alias url meower-api
+*/
